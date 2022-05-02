f06fd4ca7d1cd0cadcec10d3fe0561957b6ba9a4
Robin Boers <robindev2019@outlook.com>
2022-05-02 07:48:54 +0200

    Fix rel= me" in header h-card

--- partials/header.php
+++ partials/header.php
@@ -8,15 +8,15 @@
         <div id="card" class="md">
             <h1 class="p-name"><?= $site_author ?></h1>
             <address class="properties">
-                <a class="u-url u-uid" href="<?= $site_domain ?>"><?= $site_domain ?></a> ∙ 
+                <a rel="me" class="u-url u-uid" href="<?= $site_domain ?>"><?= $site_domain ?></a> ∙ 
                 <?php if($github_user !== "") { ?>
                     <a rel="me" href="https://github.com/<?= $github_user ?>">github</a> ∙
                 <?php } ?>
                 <?php if($twitter_user !== "") { ?>
                     <a rel="me" href="https://twitter.com/<?= $twitter_user ?>">twitter</a> ∙
                 <?php } ?>
-                <a rel="me" href="content/feed.json">json</a> ∙
-                <a rel="me" href="content/feed.php">rss</a> 
+                <a href="content/feed.json">json</a> ∙
+                <a href="content/feed.php">rss</a> 
             </address>
             <p class="p-note">
 
