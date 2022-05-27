7147cd15aaa3ea718191f1154e14747d1aa9e07c
Robin Boers <robindev2019@outlook.com>
2022-05-27 12:18:11 +0200

    Add more neutral theme

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -7,13 +7,13 @@ require "config.php";
 <section class="sidebar-section">
     <nav>
         <ul>
-            <li><a href="/">home</a></li>
-            <li><a href="/type/reply">replies</a></li>
-            <li><a href="/type/note">notes</a></li>
-            <li><a href="/type/like">likes</a></li>
-            <li><a href="/type/repost">reposts</a></li>
-            <li><a href="/type/bookmark">bookmarks</a></li>
-            <li><a href="/tag/photo">photos</a></li>
+            <li><a class="home" href="/">home</a></li>
+            <li><a class="replies" href="/type/reply">replies</a></li>
+            <li><a class="notes" href="/type/note">notes</a></li>
+            <li><a class="likes" href="/type/like">likes</a></li>
+            <li><a class="reposts" href="/type/repost">reposts</a></li>
+            <li><a class="bookmarks" href="/type/bookmark">bookmarks</a></li>
+            <li><a class="photos" href="/tag/photo">photos</a></li>
         </ul>
     </nav>
 </section>
