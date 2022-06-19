1fd63049e49916fe8f0a1ebb77cc8e3466f7f6e3
Robin Boers <robindev2019@outlook.com>
2022-06-19 16:37:08 +0200

    Downcase neopub in footer

--- partials/footer.php
+++ partials/footer.php
@@ -4,7 +4,7 @@
             <a class="get-your-own" href="https://indieweb.org/neopub">[ (っ◔◡◔)っ get your own neolog ]</a>
         </p>
         <p>
-            Neopub is built by <a href="https://geheimesite.nl">Robin Boers</a>. <a href="https://github.com/RobinBoers/neopub">Source code</a> - <a href="https://indieweb.org/neopub">Documentation</a>
+            neopub is built by <a href="https://geheimesite.nl">Robin Boers</a>. <a href="https://github.com/RobinBoers/neopub">Source code</a> - <a href="https://indieweb.org/neopub">Documentation</a>
         </p>
     </center>
 </div>
